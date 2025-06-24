4babd6978879f78579606c11d84a52de76df631b
lowergm <92257300+lowergm@users.noreply.github.com>
2025-06-24 10:48:01 -0300

    Update index.php
    Adicionando o código escrito. O código contém um site com PHP dinâmico para a validação e criação de páginas em um website

--- public/index.php
+++ public/index.php
@@ -1,17 +1,90 @@
-<!doctype html>
-<html>
+<?php
+
+$paginas = ["home"=>"", "sobre"=>""];
+
+$paginas["home"] = "<p>Inicio do Site</p>";
+
+$paginas["sobre"] = "<p>Sobre mim</p>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
   <head>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de aprendizado dinamico</title>
+    <meta charset="utf-8">
+    <style>
+      * {
+          margin: 0;
+          box-sizing: border-box;
+          padding: 0;
+          font-family: sans-serif;
+      }
+      header {
+          height: 50px;
+          background-color: #068;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+          gap: 40px;
+      }
+      
+      section {
+          max-width: 960px;
+          margin: 20px auto;
+          padding: 0 2%;
+      }
+      
+      h2 {
+          color: white;
+          padding: 8px 10px;
+          background-color: #068;
+          border-radius: 6px;
+      }
+      
+      a {
+          margin: 0 10px;
+          color: white;
+          text-decoration: none;
+          transition: 0.2s;
+      }
+      
+      a:hover {
+          color: #ccf;
+          text-decoration: underline;
+      }
+      
+      p {
+          margin-top: 10px;
+          font-size: 15pt;
+      }
+    </style>
   </head>
   <body>
-    <div class="div-main">
-        <h1>Bem Vindo!</h1>
-        <p>
-            <?php
-                echo "Este é meu primeiro Site em PHP.";
-            ?>
-        </p>
-    </div>
+    <header>
+      <?php
+      
+      foreach($paginas as $key => $value){
+          echo "<a href='?page=$key'>".ucfirst($key)."</a>";
+      }
+    
+      ?>
+    </header>
+    <section>
+      <h2><?php
+        
+        $pagina = (isset($_GET["page"]) ? $_GET["page"] : "home");
+        
+        if(!array_key_exists($pagina, $paginas)){
+            $pagina = "home";
+        }
+        
+        echo ucfirst($pagina);
+        
+      ?></h2>
+      <p><?php echo $paginas[$pagina]; ?></p>
+    </section>
   </body>
 </html>
